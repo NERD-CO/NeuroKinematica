b6e6f5e2339bdc0a398c28eabc7d74aa9caa1a9c
Erin M. Radcliffe <erin.radcliffe@cuanschutz.edu>
2023-07-21 17:28:10 -0600

    update

--- AO_Processing/Scrap_code.m
+++ AO_Processing/Scrap_code.m
@@ -5,6 +5,17 @@ cd(RawDataDir)
 Case_Matfile = dir('*.mat');             % get list of all .mat files in the current directory
 Case_MatfileNames = {Case_Matfile.name}; % create cell array containing names of the .mat files found in previous step
 
+% query rows in summaryXLSX by specific pt hemisphere/studyID (single integer correlated with StudyNum)
+% filteredXLSX = summaryXLSX(summaryXLSX.StudyNum == studyID, :);
+
+% identify rows with non-empty/non-NAN cells in the trialNum column 
+% trial_rows = ~isnan(filteredXLSX.trialNum); % logical operation
+% trial_rows = ~cellfun(@isempty, filteredXLSX.trialNum) & ... ~cellfun(@(x) any(isnan(x)), filtered_table.trialNum);
+
+% extract relevant .mat filenames in the ao_MAT_file column
+% mat_filelist = filteredXLSX.ao_MAT_file(trial_rows); % correspond with non-NAN/non-empty cells in the trialNum column
+% output = cell array of relevant .mat filenames
+
 % create struct containing only the .mat files in IO_Matfile (list of all .mat files in the studyID directory) that match the filenames in mat_filelist
 
 % initialize an empty struct to store matched files
