f031c4ac285d3e6f8222401aded31360f522a980
Erin M. Radcliffe <erin.radcliffe@cuanschutz.edu>
2023-09-20 13:40:41 -0600

    update 'run_dlcIO_processCSV' script (v3)
    success :)

--- DLC_Analysis/run_dlcIO_processCSV.m
+++ DLC_Analysis/run_dlcIO_processCSV.m
@@ -1,28 +1,26 @@
-%% Script for running 'dlcIO_processCSV' funtion
+%% Script for running 'dlcIO_processCSV2' funtion
 
 % folder for all processed dlc cases 
 outer_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc'; 
 
-% define case-specicfc file locations
-vid_dir = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\03_09_2023\video folder'; % vid_dir
-csv_dir = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\03_09_2023\csv folder'; % csv_dir 
-save_matLoc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\03_09_2023\mat folder'; % mat_dir
+% define case-specific file locations
+vid_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\03_09_2023\video folder'; % vid_dir 
+csv_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\03_09_2023\csv folder'; % csv_dir (input)
+mat_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\03_09_2023\mat folder'; % mat_dir (output)
 
 % change dir to case-specific csv folder
-% cd csv_dir
-cd 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\03_09_2023\csv folder'; % csv_dir
+cd(csv_Loc)
 
-% create cell array of all avail csv files
-csv_array = dir('*.csv'); % *list all possible .csv filetypes in dir (as struct)
-csv_array2 = {csv_array.name}; % outputs list of cell array of the csv filenames
+% create cell array of all avail csv files in csv folder
+csv_array = dir('*.csv'); % *lists all possible .csv filetypes in dir (as struct)
+csv_array2 = {csv_array.name}; % outputs list of the csv filenames (as cell array)
 
 
-% loop through all avail csv files in csv folder and run 'dlcIO_processCSV' funtion
-for i = 1:length(csv_array2)
-    csv_vidLoc = [csv_dir, filesep, csv_array2{i}];
+% loop through all avail csv files in csv folder and run 'dlcIO_processCSV2' funtion
+for csv_i = 1:length(csv_array2)
+
+    tmpCSV = [csv_Loc, filesep, csv_array2{csv_i}];
+    dlcIO_processCSV2('userLOC', tmpCSV, 'saveLOC', mat_Loc)
     
-    
-    
-    dlcIO_processCSV('dirLOC',1,'userLOC',csv_vidLoc,'saveLOC',save_matLoc)
 end
 
