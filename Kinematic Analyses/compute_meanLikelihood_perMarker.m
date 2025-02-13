8ce0ce63c63bfd13aca8d6d1a238f3af5031806b
Erin M. Radcliffe <erin.radcliffe@cuanschutz.edu>
2025-02-12 17:04:13 -0700

    update + tonight's to-do

---
+++ Kinematic Analyses/compute_meanLikelihood_perMarker.m
@@ -0,0 +1,6 @@
+%% Compute mean liklihood per marker
+
+% Data Dirs
+% Raw: Z:\RadcliffeE\Transfer_KarenG\KG_DLC Models_new\MDT5
+% Processing: Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\MDTII_5_kg
+% Analysis: Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\Kinematic Analyses
