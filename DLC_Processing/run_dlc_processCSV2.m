ccbcc806b39a817ba9b741604cfa21e047207c85
Erin M. Radcliffe <erin.radcliffe@cuanschutz.edu>
2025-02-14 15:21:24 -0700

    updates

--- DLC_Processing/run_dlc_processCSV2.m
+++ DLC_Processing/run_dlc_processCSV2.m
@@ -1,13 +1,15 @@
 %% Script for running 'dlcIO_processCSV2' funtion
 
+addpath C:\Users\erinr\OneDrive\Documents\GitHub\NeuroKinematica\DLC_Processing
+
 % folder for all processed dlc cases 
 IO_outer_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc'; 
 Clin_outer_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc';
 
-% define case-specific file locations
-vid_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\06_08_2023_RSTN\video folder'; % vid_dir 
-csv_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\06_08_2023_RSTN\csv folder'; % csv_dir (input)
-mat_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\06_08_2023_RSTN\mat folder'; % mat_dir (output)
+% define case-specific file locations (csv = input; mat = output)
+vid_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\MDTII_5_kg\MDTII_MDT5_RSTN\video folder';
+csv_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\MDTII_5_kg\MDTII_MDT5_RSTN\csv folder';
+mat_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\MDTII_5_kg\MDTII_MDT5_RSTN\mat folder';
 
 % change dir to case-specific csv folder
 cd(csv_Loc)
@@ -16,6 +18,8 @@ cd(csv_Loc)
 csv_array = dir('*.csv'); % *lists all possible .csv filetypes in dir (as struct)
 csv_array2 = {csv_array.name}; % outputs list of the csv filenames (as cell array)
 
+
+%% Run CSV-to-MAT processing function
 
 % loop through all avail csv files in csv folder and run 'dlcIO_processCSV2' funtion
 for csv_i = 1:length(csv_array2)
@@ -26,7 +30,7 @@ for csv_i = 1:length(csv_array2)
 end
 
 
-%% completed IO cases
+%% completed IO (Thesis Aim 1) cases
 
 % 03_09_2023 - LSTN (P1)
     % vid_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Intraoperative\post_DLCproc\03_09_2023\video folder'; % vid_dir 
@@ -46,7 +50,7 @@ end
 % 06_08_2023 - RSTN
 
 
-%% completed Clin cases 
+%% completed Clin (Thesis Aim 2) cases 
 
 % 09_12_2023 - LSTN (P7)
     % vid_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\09_12_2023_LSTN_v2\video folder'; % vid_dir 
@@ -57,3 +61,20 @@ end
     % vid_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\09_12_2023_RSTN_v1\video folder'; % vid_dir 
     % csv_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\09_12_2023_RSTN_v1\csv folder'; % csv_dir (input)
     % mat_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\09_12_2023_RSTN_v1\mat folder'; % mat_dir (output)
+
+%% completed Clin MDTII cases
+
+% MDTII_MDT5_LSTN
+    % vid_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\MDTII_5_kg\MDTII_MDT5_LSTN\video folder';
+    % csv_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\MDTII_5_kg\MDTII_MDT5_LSTN\csv folder';
+    % mat_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\MDTII_5_kg\MDTII_MDT5_LSTN\mat folder';
+
+% MDTII_MDT5_RSTN
+    % vid_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\MDTII_5_kg\MDTII_MDT5_RSTN\video folder';
+    % csv_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\MDTII_5_kg\MDTII_MDT5_RSTN\csv folder';
+    % mat_Loc = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\post_DLCproc\MDTII_5_kg\MDTII_MDT5_RSTN\mat folder';
+
+
+
+
+
--- Kinematic Analyses/Order_of_Operations__Clin_kinematics.m
+++ Kinematic Analyses/Order_of_Operations__Clin_kinematics.m
@@ -2,7 +2,19 @@
 
 clear all; close all; clc;
 
-% cd 'C:\Users\erinr\OneDrive\Documents\GitHub\NeuroKinematica\Kinematic Analyses'
+%% I. Processing
+
+addpath C:\Users\erinr\OneDrive\Documents\GitHub\NeuroKinematica\DLC_Processing
+
+% 1) Run CSV-to-MAT processing function: 
+% run 'run_dlc_processCSV2.m'
+
+% 2) Generate movement indices 
+% C:\Users\erinr\OneDrive\Documents\GitHub\NeuroKinematica\DLC_VideoCheck_GUI
+% run 'ER_DLC_MoceCheck_Dual_v4_mlapp
+
+%% II. Analysis 
+
 addpath 'C:\Users\erinr\OneDrive\Documents\GitHub\NeuroKinematica\Kinematic Analyses'
 
 %% 1) Define function inputs
@@ -11,15 +23,18 @@ addpath 'C:\Users\erinr\OneDrive\Documents\GitHub\NeuroKinematica\Kinematic Anal
 
 mainDir = 'Z:\RadcliffeE\Thesis_PD Neuro-correlated Kinematics\Data\Clinical\Kinematic Analyses';
 
-% Define casedate and hemisphere
+% Define caseID (date or subjectID) and hemisphere
 
-casedate_hem = '09_12_2023_LSTN';
-%casedate_hem = '09_12_2023_RSTN';
+% case_hem = '09_12_2023_LSTN';
+% case_hem = '09_12_2023_RSTN';
+
+case_hem = 'MDTII_MDT5_LSTN';
+% case_hem = 'MDTII_MDT5_RSTN';
 
 
 %% 2) Process and visualize movement timeseries data
 
-run_MovementProcessing_Clin_v2(mainDir, casedate_hem)
+run_MovementProcessing_Clin_v2(mainDir, case_hem)
 
 %% 3) by movement type
 
@@ -28,7 +43,7 @@ run_MovementProcessing_Clin_v2(mainDir, casedate_hem)
 
 %% Optional: Process and visualize movement timeseries data cleaned by artifact rejection function
 
-% run_MovementProcessing_Clin_artifactRejection(mainDir, casedate_hem)
+run_MovementProcessing_Clin_artifactRejection(mainDir, case_hem)
 
 
 %% 3) stats
@@ -41,15 +56,15 @@ hemisphere = 'L';
 switch hemisphere
     case 'L'
 
-        mainDir2 = [mainDir , filesep , '09_12_2023_LSTN'];
+        mainDir2 = [mainDir , filesep , 'MDTII_MDT5_LSTN'];
 
     case 'R'
 
-        mainDir2 = [mainDir , filesep , '09_12_2023_RSTN'];
+        mainDir2 = [mainDir , filesep , 'MDTII_MDT5_RSTN'];
 end
 
 
-run_MovementStats_Clin_v2(mainDir, casedate_hem, hemisphere)
+run_MovementStats_Clin_v2(mainDir, case_hem, hemisphere)
 
 % fix lines 87-91 in ttest_plot_2States_RandTrim s.t. output files save
 % within specified outDataDir folder (movementStats)
